f83ed3bf8625daecda11c44de11da6f07403c347
Sofia Borg <fia.borg@hotmail.com>
2022-06-15 09:34:56 +0100

    notfound updated

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,12 @@
 import { Wrapper, ContentWrapper } from "./styledComponents/StyledWrappers";
 import { Link } from "react-router-dom";
+import { StyledH1 } from "./styledComponents/StyledText";
 
 export const NotFound = () => {
   return (
     <Wrapper>
       <ContentWrapper>
-        <h1>Oops nu klev du i kobajs!</h1>
-        <Link to="/">Tillbaka</Link>
+        <StyledH1>Oops nu klev du i kobajs!</StyledH1>
       </ContentWrapper>
     </Wrapper>
   );
